6f6f55b9bbb34613f3c83bb32b50a1377de4c26f
fubenzuncc <90564493+fubenzuncc@users.noreply.github.com>
2023-02-09 18:37:20 +0800

    add mmd3d
    add mmd3d

---
+++ mmdetection3d/mmdet3d copy/models/utils/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .clip_sigmoid import clip_sigmoid
+from .mlp import MLP
+from .transformer import *
+__all__ = ['clip_sigmoid', 'MLP']
---
+++ mmdetection3d/mmdet3d/utils/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmcv.utils import Registry, build_from_cfg, print_log
+
+from .collect_env import collect_env
+from .logger import get_root_logger
+from .syncbn import convert_sync_batchnorm
+from .config import recursive_eval
+
+__all__ = ["Registry", "build_from_cfg", "get_root_logger", "print_log", "convert_sync_batchnorm", "recursive_eval"]
